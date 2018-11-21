3f6c21f78c5bf6114995063c609357d6752ff00e
fensta <fensta.git@gmail.com>
2018-11-21 17:39:37 +0300

    - added test for NOISY as tag

--- tests/test_add_tags.py
+++ tests/test_add_tags.py
@@ -4,14 +4,14 @@ from collections import Counter
 import pandas as pd
 import numpy as np
 
-from scripts.utils import add_tags, CONDITIONAL, TAG, BORDERLINE, SAFE
+from scripts.utils import add_tags, CONDITIONAL, TAG, BORDERLINE, SAFE, NOISY
 
 
 class TestAddTags(TestCase):
     """Tests add_tags() from utils.py"""
 
-    def test_add_tags(self):
-        """Add tags when using nominal and numeric features"""
+    def test_add_tags_safe_borderline(self):
+        """Add tags when using nominal and numeric features assigning borderline and safe as tags"""
         df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
@@ -45,6 +45,45 @@ class TestAddTags(TestCase):
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 3
         tagged = add_tags(df, k, class_col_name, lookup, min_max, classes)
+        # Due to floating point precision, use approximate comparison
+        self.assertTrue(tagged.equals(correct))
+
+    def test_add_tags_noisy_safe(self):
+        """Add tags when using nominal and numeric features and assigning noisy and safe as tags"""
+        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                           "C": [3, 2, 1, .5, 3, 2],
+                           "Class": ["apple", "banana", "banana", "banana", "banana", "banana"]})
+        class_col_name = "Class"
+        lookup = \
+            {
+                "A":
+                    {
+                        'high': 2,
+                        'low': 4,
+                        CONDITIONAL:
+                            {
+                                'high':
+                                    Counter({
+                                        'banana': 2
+                                    }),
+                                'low':
+                                    Counter({
+                                        'banana': 2,
+                                        'apple': 2
+                                    })
+                            }
+                    }
+            }
+        correct = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                                "C": [3, 2, 1, .5, 3, 2],
+                                "Class": ["apple", "banana", "banana", "banana", "banana", "banana"],
+                                TAG: [NOISY, SAFE, SAFE, SAFE, SAFE, SAFE]
+                                })
+        classes = ["apple", "banana"]
+        min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+        k = 3
+        tagged = add_tags(df, k, class_col_name, lookup, min_max, classes)
+        print(tagged)
         # Due to floating point precision, use approximate comparison
         self.assertTrue(tagged.equals(correct))
 
