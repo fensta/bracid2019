136d8ce4b73d04a1adc608e1942e7ab69fddcfcf
fensta <fensta.git@gmail.com>
2018-11-22 13:10:44 +0300

    - added extracted initial rules as parameter to add_tags() - adapted test cases accordingly - started to implement evaluation of new rules

--- tests/test_add_tags.py
+++ tests/test_add_tags.py
@@ -45,7 +45,15 @@ class TestAddTags(TestCase):
         classes = ["apple", "banana"]
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 3
-        tagged = add_tags(df, k, class_col_name, lookup, min_max, classes)
+        rules = [
+            pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0),
+            pd.Series({"A": "low", "B": (1, 1), "C": (2, 2), "Class": "apple"}, name=1),
+            pd.Series({"A": "high", "B": (4, 4), "C": (1, 1), "Class": "banana"}, name=2),
+            pd.Series({"A": "low", "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+            pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": "banana"}, name=4),
+            pd.Series({"A": "high", "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5)
+        ]
+        tagged = add_tags(df, k, rules, class_col_name, lookup, min_max, classes)
         # Due to floating point precision, use approximate comparison
         self.assertTrue(tagged.equals(correct))
 
@@ -83,8 +91,15 @@ class TestAddTags(TestCase):
         classes = ["apple", "banana"]
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
         k = 3
-        tagged = add_tags(df, k, class_col_name, lookup, min_max, classes)
-        print(tagged)
+        rules = [
+            pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0),
+            pd.Series({"A": "low", "B": (1, 1), "C": (2, 2), "Class": "banana"}, name=1),
+            pd.Series({"A": "high", "B": (4, 4), "C": (1, 1), "Class": "banana"}, name=2),
+            pd.Series({"A": "low", "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+            pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": "banana"}, name=4),
+            pd.Series({"A": "high", "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5)
+        ]
+        tagged = add_tags(df, k, rules, class_col_name, lookup, min_max, classes)
         # Due to floating point precision, use approximate comparison
         self.assertTrue(tagged.equals(correct))
 
@@ -122,7 +137,62 @@ class TestAddTags(TestCase):
                                 })
         classes = ["apple", "banana"]
         min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+        rules = [
+            pd.Series({"A": np.NaN, "B": (np.NaN, np.NaN), "C": (3, 3), "Class": "apple"}, name=0),
+            pd.Series({"A": np.NaN, "B": (1, 1), "C": (2, 2), "Class": "apple"}, name=1),
+            pd.Series({"A": "high", "B": (np.NaN, np.NaN), "C": (1, 1), "Class": "banana"}, name=2),
+            pd.Series({"A": np.NaN, "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+            pd.Series({"A": "low", "B": (np.NaN, np.NaN), "C": (3, 3), "Class": "banana"}, name=4),
+            pd.Series({"A": np.NaN, "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5)
+        ]
         k = 3
-        tagged = add_tags(df, k, class_col_name, lookup, min_max, classes)
+        tagged = add_tags(df, k, rules, class_col_name, lookup, min_max, classes)
+        # Due to floating point precision, use approximate comparison
+        self.assertTrue(tagged.equals(correct))
+
+    def test_add_tags_all_tags(self):
+        """Add tags when using nominal and numeric features and assigning noisy, borderline and safe as tags"""
+        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                           "C": [3, 2, 1, .5, 3, 2],
+                           "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+        class_col_name = "Class"
+        lookup = \
+            {
+                "A":
+                    {
+                        'high': 2,
+                        'low': 4,
+                        CONDITIONAL:
+                            {
+                                'high':
+                                    Counter({
+                                        'banana': 2
+                                    }),
+                                'low':
+                                    Counter({
+                                        'banana': 2,
+                                        'apple': 2
+                                    })
+                            }
+                    }
+            }
+        correct = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                                "C": [3, 2, 1, .5, 3, 2],
+                                "Class": ["apple", "banana", "banana", "banana", "banana", "banana"],
+                                TAG: [NOISY, SAFE, SAFE, SAFE, SAFE, SAFE]
+                                })
+        classes = ["apple", "banana"]
+        min_max = pd.DataFrame({"C": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+        k = 3
+        rules = [
+            pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0),
+            pd.Series({"A": "low", "B": (1, 1), "C": (2, 2), "Class": "apple"}, name=1),
+            pd.Series({"A": "high", "B": (4, 4), "C": (1, 1), "Class": "banana"}, name=2),
+            pd.Series({"A": "low", "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+            pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": "banana"}, name=4),
+            pd.Series({"A": "high", "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5)
+        ]
+        tagged = add_tags(df, k, rules, class_col_name, lookup, min_max, classes)
+        print(tagged)
         # Due to floating point precision, use approximate comparison
         self.assertTrue(tagged.equals(correct))
