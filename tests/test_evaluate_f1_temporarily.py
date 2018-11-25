747ff01e66f419f1db853f94252a43718562946f
fensta <fensta.git@gmail.com>
2018-11-26 00:15:15 +0300

    - fixed test

--- tests/test_evaluate_f1_temporarily.py
+++ tests/test_evaluate_f1_temporarily.py
@@ -12,7 +12,7 @@ class TestEvaluateF1Temporarily(TestCase):
     """Tests evaluate_f1_temporarily() in utils.py"""
 
     def test_evaluate_f1_temporarily(self):
-        """Tests that the global confusion matrix won't be updated despite local changes"""
+        """Tests that the global variables won't be updated despite local changes"""
         df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3, 2],
                            "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
@@ -50,12 +50,6 @@ class TestEvaluateF1Temporarily(TestCase):
         correct_closest = copy.deepcopy(my_vars.closest_rule_per_example)
         my_vars.conf_matrix = {my_vars.TP: {0, 1}, my_vars.FP: set(), my_vars.TN: {2, 5}, my_vars.FN: {3, 4}}
         new_rule = pd.Series({"A": "low", "B": (0.5, 1.0), "C": (3, 3), "Class": "banana"}, name=0)
-        # tagged, initial_rules = add_tags_and_extract_rules(df, 2, class_col_name, lookup, min_max, classes)
-        # print("init rules")
-        # print(rules)
-        # print("dataset")
-        # print(df)
-        # print(my_vars.seed_mapping)
         correct_f1 = 0.8
 
         f1, conf_matrix, closest = evaluate_f1_temporarily(df, new_rule, class_col_name, lookup, min_max, classes)
@@ -69,18 +63,15 @@ class TestEvaluateF1Temporarily(TestCase):
             3: (1, 0.038125),
             4: (0, 0.0),
             5: (2, 0.67015625)}
-        # self.assertTrue(f1 == correct_f1)
-        print("deep copy closest rule per example")
-        print(correct_closest)
-        print("original")
-        print(my_vars.closest_rule_per_example)
-        print("local")
-        print(closest)
-        print("local conf matrix")
-        print(conf_matrix)
-        print("global conf matrix")
-        print(my_vars.conf_matrix)
-        for example_id in my_vars.closest_rule_per_example:
-            rule_id, dist = my_vars.closest_rule_per_example[example_id]
+        self.assertTrue(f1 == correct_f1)
+        # Local result is still the same as in test_evaluate_f1_update_confusion_matrix.py
+        for example_id in closest:
+            rule_id, dist = closest[example_id]
             self.assertTrue(rule_id == correct_closest_rule_per_example[example_id][0] and
                             abs(dist - correct_closest_rule_per_example[example_id][1]) < 0.001)
+        correct_conf_matrix = {'tp': {0, 1}, 'fp': set(), 'tn': {2, 4, 5}, 'fn': {3}}
+        self.assertTrue(conf_matrix == correct_conf_matrix)
+        # But now check that global variables remained unaffected by the changes
+        correct_conf_matrix = {'tp': {0, 1}, 'fp': set(), 'tn': {2, 5}, 'fn': {3, 4}}
+        self.assertTrue(my_vars.conf_matrix == correct_conf_matrix)
+        self.assertTrue(correct_closest == my_vars.closest_rule_per_example)
