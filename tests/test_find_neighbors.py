750819b847a10411c59cabc938df64b3fc555d0a
fensta <fensta.git@gmail.com>
2018-12-02 22:42:00 +0300

    - added a new test case to test the new parameter in find_nearest_examples()

--- tests/test_find_neighbors.py
+++ tests/test_find_neighbors.py
@@ -88,6 +88,50 @@ class TestFindNeighbors(TestCase):
         print(correct)
         self.assertTrue(neighbors.equals(correct))
 
+    def test_find_neighbors_numeric_nominal_label_type(self):
+        """Tests what happens if input has a numeric and a nominal feature and we vary label_type as parameter"""
+        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                           "C": [3, 2, 1, .5, 3, 2],
+                           "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+        class_col_name = "Class"
+        lookup = \
+            {
+                "A":
+                    {
+                        'high': 2,
+                        'low': 4,
+                        my_vars.CONDITIONAL:
+                            {
+                                'high':
+                                    Counter({
+                                        'banana': 2
+                                    }),
+                                'low':
+                                    Counter({
+                                        'banana': 2,
+                                        'apple': 2
+                                    })
+                            }
+                    }
+            }
+        k = 3
+        correct_all = df.iloc[[5, 2, 0]]
+        correct_same = df.iloc[[5, 2, 3]]
+        correct_opposite = df.iloc[[0, 1]]
+        rule = pd.Series({"A": "high", "B": (1, 1), "Class": "banana"})
+        classes = ["apple", "banana"]
+        min_max = pd.DataFrame({"A": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+        neighbors_all, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
+                                                 label_type=my_vars.ALL_LABELS, only_uncovered_neighbors=False)
+        neighbors_same, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
+                                                  label_type=my_vars.SAME_LABEL_AS_RULE, only_uncovered_neighbors=False)
+        neighbors_opposite, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
+                                                      label_type=my_vars.OPPOSITE_LABEL_TO_RULE,
+                                                      only_uncovered_neighbors=False)
+        self.assertTrue(neighbors_all.equals(correct_all))
+        self.assertTrue(neighbors_same.equals(correct_same))
+        self.assertTrue(neighbors_opposite.equals(correct_opposite))
+
     def test_find_neighbors_numeric_nominal_covered(self):
             """Tests what happens if input has a numeric and a nominal feature and some examples are already covered
             by the rule"""
