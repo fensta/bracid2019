f24db9663d1fd3e3c87bbacf86710b507adadc87
fensta <fensta.git@gmail.com>
2018-12-04 00:42:46 +0300

    - updated the test cases for extend_rule()

--- tests/test_extend_rule.py
+++ tests/test_extend_rule.py
@@ -10,8 +10,8 @@ import scripts.vars as my_vars
 class TestExtendRule(TestCase):
     """Tests extend_rule() from utils.py"""
 
-    def test_extend_rule_only_nominal(self):
-        """Test that a rule containing only nominal features is extended correctly"""
+    def test_extend_rule_mixed(self):
+        """Test that a rule containing nominal and numeric features is extended correctly"""
         df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
                            "C": [3, 2, 1, .5, 3.1, 3.2],
                            "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
@@ -51,14 +51,47 @@ class TestExtendRule(TestCase):
         k = 3
         extended_rule = extend_rule(df, k, rules[0], class_col_name, lookup, min_max, classes)
         correct_rule = pd.Series({"A": "low", "B": (0.875, 1.25), "C": (1.75, 3.05), "Class": "apple"}, name=0)
-        print("extended rule")
-        print(extended_rule)
         self.assertTrue(extended_rule.equals(correct_rule))
 
-    def test_extend_rule_only_numeric(self):
-        """Test that a rule containing only numeric features is extended correctly"""
-        self.fail()
+    def test_extend_rule_no_change(self):
+        """Test that a rule containing nominal and numeric features isn't extended due to no neighbors"""
+        df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 1, 1, 1, 1],
+                           "C": [3, 2, 3, 3, 3, 3],
+                           "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+        class_col_name = "Class"
+        lookup = \
+            {
+                "A":
+                    {
+                        'high': 2,
+                        'low': 4,
+                        my_vars.CONDITIONAL:
+                            {
+                                'high':
+                                    Counter({
+                                        'banana': 2
+                                    }),
+                                'low':
+                                    Counter({
+                                        'banana': 2,
+                                        'apple': 2
+                                    })
+                            }
+                    }
+            }
+        classes = ["apple", "banana"]
+        min_max = pd.DataFrame({"B": {"min": 1, "max": 5}, "C": {"min": 1, "max": 11}})
+        my_vars.positive_class = "apple"
+        rules = [
+            pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0),
+            pd.Series({"A": "low", "B": (1, 1), "C": (2, 2), "Class": "apple"}, name=1),
+            pd.Series({"A": "high", "B": (4, 4), "C": (1, 1), "Class": "banana"}, name=2),
+            pd.Series({"A": "low", "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+            pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": "banana"}, name=4),
+            pd.Series({"A": "high", "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5)
+        ]
 
-    def test_extend_rule_only_numeric(self):
-        """Test that a rule containing nominal and numeric features is extended correctly"""
-        self.fail()
+        k = 3
+        extended_rule = extend_rule(df, k, rules[0], class_col_name, lookup, min_max, classes)
+        correct_rule = pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0)
+        self.assertTrue(extended_rule.equals(correct_rule))
