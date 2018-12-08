d66bd59f295ffbeff8fcbfdec98fdbcb32442bb1
fensta <fensta.git@gmail.com>
2018-12-08 13:59:48 +0300

    - added new test case to check that the best generalized rule is unique and not a duplicate of an existing rule - if it's a duplicate, it's not entered and the corresponding statistics are updated, i.e. the duplicate is removed

--- tests/test_add_one_best_rule.py
+++ tests/test_add_one_best_rule.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 import pandas as pd
 
-from scripts.utils import add_one_best_rule, find_nearest_examples
+from scripts.utils import add_one_best_rule, find_nearest_examples, compute_hash
 import scripts.vars as my_vars
 
 
@@ -151,3 +151,96 @@ class TestAddOneBestRule(TestCase):
                             abs(dist - correct_closest_rule_per_example[example_id][1]) < 0.001)
         self.assertTrue(rules[0].equals(correct_generalized_rule))
         self.assertTrue(my_vars.conf_matrix == correct_confusion_matrix)
+
+    def test_add_one_best_rule_unique(self):
+            """Tests that the best rule found by this function is unique and correspondingly updates relevant
+            statistics if that's not the case"""
+            df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                               "C": [3, 2, 1, .5, 3, 2],
+                               "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+            class_col_name = "Class"
+            lookup = \
+                {
+                    "A":
+                        {
+                            'high': 2,
+                            'low': 4,
+                            my_vars.CONDITIONAL:
+                                {
+                                    'high':
+                                        Counter({
+                                            'banana': 2
+                                        }),
+                                    'low':
+                                        Counter({
+                                            'banana': 2,
+                                            'apple': 2
+                                        })
+                                }
+                        }
+                }
+            classes = ["apple", "banana"]
+            min_max = pd.DataFrame({"B": {"min": 1, "max": 5}, "C": {"min": 1, "max": 11}})
+            my_vars.positive_class = "apple"
+            # name=6 because this guy already exists in the rules and the new rule with name=0 becomes the same, so
+            # it's removed
+            correct_generalized_rule = pd.Series({"A": "low", "B": (1, 1), "C": (2.0, 3), "Class": "apple"}, name=6)
+            rules = [
+                pd.Series({"A": "low", "B": (1, 1), "C": (3, 3), "Class": "apple"}, name=0),
+                pd.Series({"A": "low", "B": (1, 1), "C": (2, 2), "Class": "apple"}, name=1),
+                pd.Series({"A": "high", "B": (4, 4), "C": (1, 1), "Class": "banana"}, name=2),
+                pd.Series({"A": "low", "B": (1.5, 1.5), "C": (0.5, 0.5), "Class": "banana"}, name=3),
+                pd.Series({"A": "low", "B": (0.5, 0.5), "C": (3, 3), "Class": "banana"}, name=4),
+                pd.Series({"A": "high", "B": (0.75, 0.75), "C": (2, 2), "Class": "banana"}, name=5),
+                pd.Series({"A": "low", "B": (1, 1), "C": (2.0, 3), "Class": "apple"}, name=6)  # same as best rule
+            ]
+            for rule in rules:
+                rule_hash = compute_hash(rule)
+                my_vars.unique_rules[rule_hash] = rule.name
+            correct_generalized_rule_hash = compute_hash(correct_generalized_rule)
+            my_vars.seed_rule_example = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+            my_vars.seed_example_rule = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+            my_vars.closest_examples_per_rule = {0: {1, 4}, 1: {0, 3}, 2: {5}, 5: {2}}
+            # Note that 6: {8} is incorrect and was just added to test if the entries are merged correctly
+            my_vars.examples_covered_by_rule = {0: {0}, 1: {1}, 2: {2}, 3: {3}, 4: {4}, 5: {5}, 6: {8}}
+            print("rule hashes", my_vars.unique_rules)
+            print(correct_generalized_rule_hash)
+            my_vars.closest_rule_per_example = {
+                0: (1, 0.010000000000000002),
+                1: (0, 0.010000000000000002),
+                2: (5, 0.67015625),
+                3: (1, 0.038125),
+                4: (0, 0.015625),
+                5: (2, 0.67015625),
+                8: (6, 12.543)  # Fake entry
+            }
+            # Actually, correctly it should've been
+            # my_vars.conf_matrix = {my_vars.TP: {0, 1}, my_vars.FP: set(), my_vars.TN: {2, 5}, my_vars.FN: {3, 4}}
+            # at the start (i.e. F1=0.66666), but to see if it changes, it's changed
+            my_vars.conf_matrix = {my_vars.TP: {0}, my_vars.FP: set(), my_vars.TN: {1, 2, 5}, my_vars.FN: {3, 4}}
+            initial_f1 = 0.1
+            k = 3
+            neighbors, dists = find_nearest_examples(df, k, rules[0], class_col_name, lookup, min_max, classes,
+                                                     label_type=my_vars.SAME_LABEL_AS_RULE,
+                                                     only_uncovered_neighbors=True)
+            improved, updated_rules = add_one_best_rule(df, neighbors, rules[0], rules, initial_f1, class_col_name,
+                                                        lookup,
+                                                        min_max, classes)
+            correct_closest_rule_per_example = {
+                0: (1, 0.010000000000000002),
+                1: (6, 0.0),
+                2: (5, 0.67015625),
+                3: (1, 0.038125),
+                4: (6, 0.015625),
+                5: (2, 0.67015625)}
+            self.assertTrue(improved is True)
+            correct_confusion_matrix = {my_vars.TP: {0, 1}, my_vars.FP: set(), my_vars.TN: {2, 5}, my_vars.FN: {3, 4}}
+            # Make sure confusion matrix, closest rule per example, and rule set were updated with the updated rule too
+            for example_id in my_vars.closest_rule_per_example:
+                # 8 was only added to test something else, since it won't be in the result
+                if example_id != 8:
+                    rule_id, dist = my_vars.closest_rule_per_example[example_id]
+                    self.assertTrue(rule_id == correct_closest_rule_per_example[example_id][0] and
+                                    abs(dist - correct_closest_rule_per_example[example_id][1]) < 0.001)
+            self.assertTrue(rules[5].equals(correct_generalized_rule))
+            self.assertTrue(my_vars.conf_matrix == correct_confusion_matrix)
