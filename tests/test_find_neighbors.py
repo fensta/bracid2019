d92440ca5ce829d4ff0533d3b041058ffb7b9934
fensta <fensta.git@gmail.com>
2018-12-11 14:05:41 +0300

    - added a new test case for checking that the global statistics for closest example per rule are updated correctly

--- tests/test_find_neighbors.py
+++ tests/test_find_neighbors.py
@@ -179,3 +179,106 @@ class TestFindNeighbors(TestCase):
             neighbors, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
                                                  label_type=my_vars.SAME_LABEL_AS_RULE, only_uncovered_neighbors=True)
             self.assertTrue(neighbors.equals(correct))
+
+    def test_find_neighbors_numeric_nominal_stats(self):
+            """Tests that global statistics are updated accordingly"""
+            df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                               "C": [3, 2, 1, .5, 3, 2],
+                               "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+            class_col_name = "Class"
+            lookup = \
+                {
+                    "A":
+                        {
+                            'high': 2,
+                            'low': 4,
+                            my_vars.CONDITIONAL:
+                                {
+                                    'high':
+                                        Counter({
+                                            'banana': 2
+                                        }),
+                                    'low':
+                                        Counter({
+                                            'banana': 2,
+                                            'apple': 2
+                                        })
+                                }
+                        }
+                }
+            my_vars.closest_rule_per_example = {
+                0: (1, 0.010000000000000002),
+                1: (0, 0.010000000000000002),
+                2: (5, 0.67015625),
+                3: (1, 0.038125),
+                4: (0, 0.015625),
+                5: (2, 0.67015625)}
+            my_vars.closest_examples_per_rule = {0: {1, 4}, 1: {0, 3}, 2: {5}, 5: {2}}
+            k = 4
+            correct = df.iloc[[5, 2, 3, 4]]
+            rule = pd.Series({"A": "high", "B": (1, 1), "Class": "banana"}, name=0)
+            classes = ["apple", "banana"]
+            my_vars.positive_class = "banana"
+            min_max = pd.DataFrame({"A": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+            correct_covered = {}
+            correct_examples_per_rule = {0: {1, 2, 4, 5}, 1: {0, 3}}
+            correct_closest_rule_per_example = {
+                0: (1, 0.010000000000000002), 1: (0, 0.010000000000000002), 2: (0, 0.09), 3: (1, 0.038125),
+                4: (0, 0.015625), 5: (0, 0.0006250000000000001)}
+            neighbors, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
+                                                 label_type=my_vars.SAME_LABEL_AS_RULE, only_uncovered_neighbors=False)
+            self.assertTrue(neighbors.equals(correct))
+            self.assertTrue(correct_covered == my_vars.examples_covered_by_rule)
+            self.assertTrue(correct_examples_per_rule == my_vars.closest_examples_per_rule)
+            for example_id, (rule_id, dist) in correct_closest_rule_per_example.items():
+                self.assertTrue(example_id in my_vars.closest_rule_per_example)
+                other_id, other_dist = my_vars.closest_rule_per_example[example_id]
+                self.assertTrue(rule_id == other_id)
+                self.assertTrue(abs(dist-other_dist) < 0.0001)
+
+    def test_find_neighbors_numeric_nominal_covers(self):
+            """Tests that the stats for a newly covered rule are updated (dist = 0)"""
+            df = pd.DataFrame({"A": ["low", "low", "high", "low", "low", "high"], "B": [1, 1, 4, 1.5, 0.5, 0.75],
+                               "C": [3, 2, 1, .5, 3, 2],
+                               "Class": ["apple", "apple", "banana", "banana", "banana", "banana"]})
+            class_col_name = "Class"
+            lookup = \
+                {
+                    "A":
+                        {
+                            'high': 2,
+                            'low': 4,
+                            my_vars.CONDITIONAL:
+                                {
+                                    'high':
+                                        Counter({
+                                            'banana': 2
+                                        }),
+                                    'low':
+                                        Counter({
+                                            'banana': 2,
+                                            'apple': 2
+                                        })
+                                }
+                        }
+                }
+            k = 4
+            correct = None
+            if k == 1:
+                correct = df.iloc[[5]]
+            elif k == 2:
+                correct = df.iloc[[5, 2]]
+            elif k == 3:
+                correct = df.iloc[[5, 2, 3]]
+            elif k >= 4:
+                # correct = df.iloc[[5, 2, 3, 4]]
+                # Examples at indices 2 and 4 are already covered by the rule, so don't return them as neighbors
+                my_vars.examples_covered_by_rule = {0: {2, 4}}
+                correct = df.iloc[[5, 3]]
+            rule = pd.Series({"A": "high", "B": (1, 1), "Class": "banana"}, name=0)
+            classes = ["apple", "banana"]
+            min_max = pd.DataFrame({"A": {"min": 1, "max": 5}, "B": {"min": 1, "max": 11}})
+
+            neighbors, _ = find_nearest_examples(df, k, rule, class_col_name, lookup, min_max, classes,
+                                                 label_type=my_vars.SAME_LABEL_AS_RULE, only_uncovered_neighbors=True)
+            self.assertTrue(neighbors.equals(correct))
